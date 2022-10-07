efe7e2c4c701755c3d2383e200f8c3018cebf59d
RodrigoCBArj <rodrigocbarj@gmail.com>
2022-10-07 15:29:50 -0300

    removing null attributes

--- src/main/java/br/com/rodrigocbarj/cloudparking/controller/dto/ParkingDTO.java
+++ src/main/java/br/com/rodrigocbarj/cloudparking/controller/dto/ParkingDTO.java
@@ -1,7 +1,10 @@
 package br.com.rodrigocbarj.cloudparking.controller.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import java.time.LocalDateTime;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ParkingDTO {
 
     private Long id;
