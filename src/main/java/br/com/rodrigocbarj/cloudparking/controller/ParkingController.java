41b5b61f6093dfacff23c2ff525893176c210828
RodrigoCBArj <rodrigocbarj@gmail.com>
2022-11-01 08:23:11 -0300

    implementing swagger

--- src/main/java/br/com/rodrigocbarj/cloudparking/controller/ParkingController.java
+++ src/main/java/br/com/rodrigocbarj/cloudparking/controller/ParkingController.java
@@ -5,6 +5,8 @@ import br.com.rodrigocbarj.cloudparking.controller.dto.ParkingDTO;
 import br.com.rodrigocbarj.cloudparking.controller.mapper.ParkingMapper;
 import br.com.rodrigocbarj.cloudparking.model.Parking;
 import br.com.rodrigocbarj.cloudparking.service.ParkingService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +15,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/parking")
+@Api(tags = "Parking Controller")
 public class ParkingController {
 
     private final ParkingService parkingService;
@@ -25,18 +28,21 @@ public class ParkingController {
     }
 
     @GetMapping
+    @ApiOperation("Find all Parkings")
     public ResponseEntity<List<ParkingDTO>> findAll() {
         List<Parking> parkingList = parkingService.findAll();
         return ResponseEntity.ok(parkingMapper.toParkingDTOList(parkingList));
     }
 
     @GetMapping("/{id}")
+    @ApiOperation("Find Parking by ID")
     public ResponseEntity<ParkingDTO> findById(@PathVariable Long id) {
         Parking parking = parkingService.findById(id);
         return ResponseEntity.ok(parkingMapper.toParkingDTO(parking));
     }
 
     @PostMapping
+    @ApiOperation("Create new Parking")
     public ResponseEntity<ParkingDTO> create(@RequestBody ParkingCreateDTO createDTO) {
         Parking newParking = parkingService.create(parkingMapper.toParkingByCreate(createDTO));
         return ResponseEntity.status(HttpStatus.CREATED)
